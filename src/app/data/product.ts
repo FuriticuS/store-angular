312abd23963412d18b3d9d3a30241726467fd3a4
Fur1ouS <Fur1ouS@ya.ru>
2023-03-25 05:12:10 +0300

    add product data

---
+++ src/app/data/product.ts
@@ -0,0 +1,44 @@
+export const products = [
+  {
+    id: 1,
+    name: "Non sed incidunt dolor.",
+    description: "Perferendis possimus voluptas tempora eius. Aut molestias modi suscipit ullam. Voluptatem excepturi ipsa recusandae dolorem.",
+    ean: "9964026430849",
+    upc: "150458448755",
+    image: "http://placeimg.com/640/480/tech",
+    images: [
+      {
+        "title": "Et aut facilis et quia sit.",
+        "description": "Voluptatibus id sequi dicta quibusdam. Ipsam et earum non aliquam unde ut qui. Iure quia illo dolore ipsa maiores autem.",
+        "url": "http://placeimg.com/640/480/any"
+      },
+      {
+        "title": "In enim magni adipisci et.",
+        "description": "Quia nihil ipsum non et. Alias perspiciatis itaque similique. Enim quasi occaecati ducimus occaecati dolor vitae. Sit aut aperiam at.",
+        "url": "http://placeimg.com/640/480/any"
+      },
+      {
+        "title": "Officia dolorum error est.",
+        "description": "Ab dolorem accusantium sed id adipisci qui dolore. Harum placeat aut consectetur omnis. Reiciendis libero libero laudantium et quasi quo voluptatem ipsa.",
+        "url": "http://placeimg.com/640/480/any"
+      }
+    ],
+    net_price: 4510.63,
+    taxes: "12",
+    price: "5051.91",
+    categories: [
+      "96f6cd2f-a328-38cf-919b-dc798449d5e8",
+      "b2e0f55d-e2fc-36bb-960a-19dd81ddf8fe",
+      "7ca6380d-7279-324c-9e2c-f59af1028c09",
+      "5e0714c4-d7d3-3587-a781-db4df7af55c3"
+    ],
+    tags: [
+      "saepe",
+      "adipisci",
+      "voluptatibus",
+      "nostrum",
+      "doloremque",
+      "sit"
+    ]
+  }
+]
